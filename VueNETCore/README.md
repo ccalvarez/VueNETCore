c798bb9c1c040548063eb02665b805b5383031b7
Carolina Cordero <ccalvarez@gmail.com>
2020-03-31 16:58:58 -0600

    Vue app consuming Web API REST endpoint

---
+++ VueNETCore/README.md
@@ -0,0 +1,6 @@
+# VueNETCore
+How to integrate Vue.js and ASP.NET Core using SPA Extension
+
+- Vue app and .NET Core 3.1.3 Web API in the same Visual Studio solution (Visual Studio 2019)
+- Visual Studio _Start Debugging_ starts Vue app
+- No hot realoading yet
---
+++ VueNETCore/client-app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-bind:key="r" v-for="r in this.results">{{ r }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  async beforeMount() {
+    const r = await fetch('/api/values');
+    this.results = await r.json();
+  },
+};
+</script>
